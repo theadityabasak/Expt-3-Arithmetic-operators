af336d8a8f3138e5006a74ae8eb0885364518319
theadityabasak <orandom46@gmail.com>
2024-11-05 20:28:54 +0530

    Add files via upload

---
+++ Arithmetic operators.cpp
@@ -0,0 +1,25 @@
+// 23070123007 aditya basak
+#include <iostream>
+using namespace std;
+int main() {
+    float a,b,sum,sub,multiply,division;
+    cout<<"enter a:"<<endl;
+    cin>>a;
+    cout<<"enter b:"<<endl;
+    cin>>b;
+    sum=a+b;
+    sub=a-b;
+    multiply=a*b;
+    division=a/b;
+    // Write C++ code here
+    cout << "sum is"<<sum<<endl;
+    cout << "sub is"<<sub<<endl;
+    cout << "multiply is"<<multiply<<endl;
+    cout << "division is"<<division<<endl;
+// output 
+// a=5.0
+// b=2.0
+// sum is7.0
+// sub is3.0
+// multiply is10.0
+// division is2.5
